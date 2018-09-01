d46dc2e2207b60b0e64d501236ddeb507def9d04
rakesh singh <rakesh.singh@tum.de>
2018-09-01 02:07:43 +0200

    added the spreadsheet cell files for the new exercise on chapter 8

--- Professional_c++_notes_4ed.cpp
+++ Professional_c++_notes_4ed.cpp
@@ -2,3 +2,25 @@
 // gcc -lstdc++ -std=c++17 -o <executable name> <source cpp1> ....
 
 
+/*
+ * saveRecord()
+ * 
+ * Saves the given record to the database.
+ *
+ * Parameters:
+ *    Record& record: the record to save to the database.
+ * Returns: int
+ *    An integer representing the ID of the saved record.
+ * Throws:
+ *    DatabaseNotOpenedException if the openDatabase() method has not
+ *    been called yet.
+ */
+int saveRecord(Record& record);
+
+  /**
+         * Computes the seed ratio, using the Marigold algorithm.
+         * @param slowCalc Whether or not to use long (slow) calculations
+         * @return The marigold ratio
+         */
+
+        
---
+++ SpreadsheetCell.h
@@ -0,0 +1,20 @@
+//SpreadsheetCell.h - in which each cell can store a single number
+//                    can contain string which needs to be converted to a text value otherwise a 0 is stored.
+#pragma once
+#include <string>
+#include <string_view>
+
+class SpreadsheetCell
+{
+    public:
+        void setValue(double inValue);
+        double getValue() const;
+
+        //if compiler is not c++17 complatible then string_view should be replaced by std::string&
+        void setString(std::string_view inString);
+        std::string getString() const;
+    private:
+        std::string doubleToString(double inValue) const;
+        double stringToDouble(std::string_view inString) const;
+        double mValue;
+};
